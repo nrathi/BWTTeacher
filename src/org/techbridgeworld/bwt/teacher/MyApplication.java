01eb15d79739129662ceda6ffdc86a6193113034
Neha Rathi <neharrathi@gmail.com>
2013-04-28 16:49:18 -0400

    More comments.

--- src/org/techbridgeworld/bwt/teacher/MyApplication.java
+++ src/org/techbridgeworld/bwt/teacher/MyApplication.java
@@ -84,12 +84,15 @@ public class MyApplication extends Application implements
 				speakOut(help);
 			}
 		});
+		
+		// Populate hangmanWords
 		new HTTPAsyncTask().execute();
 	}
 
 	@Override
 	public void onInit(int status) {
 		if (status == TextToSpeech.SUCCESS) {
+			// Set up myTTS
 			int result = myTTS.setLanguage(Locale.US);
 			if (result == TextToSpeech.LANG_MISSING_DATA
 					|| result == TextToSpeech.LANG_NOT_SUPPORTED)
